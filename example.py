2c6e80c5f6dab01a4d309ab45e8489ad8262ae35
Pål Grønås Drange <pgdr@statoil.com>
2017-02-28 20:42:16 +0100

    added polyregression of friskby data

--- example.py
+++ example.py
@@ -1,39 +1,62 @@
-import pandas as pd, matplotlib.pyplot as plt, sklearn, numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def analysis(arr):
+    print('%.1f +/- %.1f (%.1f %.1f %.1f %.1f %.1f)'
+          % (arr.mean(), arr.std(),
+          arr.min(),
+          np.percentile(arr, 10), np.percentile(arr, 50), np.percentile(arr, 90),
+          arr.max()))
 
 df = pd.read_csv('data/pm.csv', delimiter=';')
-vals = df['value']
-arr = np.array(vals)
-
-arr.mean()
-arr.min()
-arr.max()
-np.percentile(arr, 10)
-np.percentile(arr, 90)
-
 pm10 = df[(df.sensor == 'PM10')]
 pm25 = df[(df.sensor == 'PM25')]
 
-plt.figure()
-plt.plot(np.array(pm10['value']))
-plt.figure()
-plt.plot(np.array(pm25['value']))
+arr_10 = np.array(pm10['value'])
+arr_25 = np.array(pm25['value'])
+analysis(arr_10)
+analysis(arr_25)
 
-#mtx = np.array([(int(x[1][3].split()[1].split(':')[0]), float(x[1][1]))
-#                   for x in pm10.iterrows()])
+plt.figure()
+plt.plot(np.array(pm10['value']), color='b')
+plt.plot(np.array(pm25['value']), color='r')
 
 times_pm10 = np.array([int(x[1][3].split()[1].split(':')[0]) for x in pm10.iterrows()])
 vals_pm10  = np.array([float(x[1][1]) for x in pm10.iterrows()])
 
-
-plt.figure()
-plt.scatter(times_pm10, vals_pm10)
-
-
-
 times_pm25 = np.array([int(x[1][3].split()[1].split(':')[0]) for x in pm25.iterrows()])
 vals_pm25  = np.array([float(x[1][1]) for x in pm25.iterrows()])
 
-
 plt.figure()
-plt.scatter(times_pm25, vals_pm25)
+plt.scatter(times_pm10, vals_pm10, color='b')
+plt.scatter(times_pm25, vals_pm25, color='r')
+
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+pred = {}
+for i in range(len(times_pm10)):
+    t,v = times_pm10[i], vals_pm10[i]
+    if t not in pred:
+        pred[t] = []
+    pred[t].append(v)
+
+keys = np.array(pred.keys())
+vals = []
+for k in keys:
+    vals.append(np.array(pred[k]).mean())
+print keys, vals
+
+
+keys = keys.reshape((24,1))
+
+model = make_pipeline(PolynomialFeatures(5), Ridge())
+
+model.fit(keys, vals)
+prediction_keys = [i/3.0 for i in range(24*3)]
+prediction_vals = [model.predict(i/3.0) for i in range(24*3)]
+plt.scatter(prediction_keys, prediction_vals, color='g')
 plt.show()
